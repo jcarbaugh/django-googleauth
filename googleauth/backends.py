db679eec86e547dd2d30f89493009034195e705c
Dan Drinkard <dan.drinkard@gmail.com>
2013-03-12 13:29:41 -0400

    add setting to automatically add google auth users to groups

--- googleauth/backends.py
+++ googleauth/backends.py
@@ -1,34 +1,44 @@
 from django.conf import settings
-from django.contrib.auth.models import User
+from django.contrib.auth.models import User, Group
 
 IS_STAFF = getattr(settings, 'GOOGLEAUTH_IS_STAFF', True)
+USER_GROUPS = getattr(settings, 'GOOGLEAUTH_GROUPS', Tuple())
 DOMAIN = getattr(settings, 'GOOGLEAUTH_DOMAIN', None)
 
+
 class GoogleAuthBackend(object):
-    
+
     def authenticate(self, identifier=None, attributes=None):
-        
+
         email = attributes.get('email', None)
         (username, domain) = email.split('@')
-        
+
         if DOMAIN and DOMAIN != domain:
             return None
-        
+
         try:
-            
+
             user = User.objects.get(email=email)
-            
+
         except User.DoesNotExist:
-            
+
             user = User.objects.create(username=username, email=email)
             user.first_name = attributes.get('firstname', None)
             user.last_name = attributes.get('lastname', None)
             user.is_staff = IS_STAFF
             user.set_unusable_password()
+
+            for group in USER_GROUPS:
+                try:
+                    grp = Group.objects.get(name=group)
+                    user.groups.add(grp)
+                except:
+                    pass
+
             user.save()
-        
+
         return user
-    
+
     def get_user(self, user_id):
         try:
             return User.objects.get(pk=user_id)
