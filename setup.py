bbb0520a978482ed636955b7c0fb5707a700661d
Jeremy Carbaugh <jcarbaugh@gmail.com>
2015-02-18 12:18:23 -0500

    add requests requirement

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ long_description = open('README.rst').read()
 
 setup(
     name="django-googleauth",
-    version='2.0',
+    version='2.1',
     packages=["googleauth"],
     description="OAuth 2.0 authentication for Google and Google Apps accounts",
     url="https://github.com/jcarbaugh/django-googleauth",
@@ -13,7 +13,10 @@ setup(
     license='BSD',
     long_description=long_description,
     platforms=["any"],
-    install_requires=["PyJWT==0.2.1"],
+    install_requires=[
+        "PyJWT==0.4.1",
+        "requests==2.5.1",
+    ],
     classifiers=[
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
